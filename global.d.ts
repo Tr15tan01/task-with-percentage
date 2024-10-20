57b082d851262c3d493f31686f490cea6223412e
tr15tan01 <t.varamashvili@gmail.com>
2024-10-20 23:33:15 +0400

    refactored to fix typescript errors

--- global.d.ts
+++
@@ -1,8 +0,0 @@
-// global.d.ts
-
-import { PrismaClient } from "@prisma/client";
-
-declare global {
-  // Extend the global namespace to include a prisma property
-  let prisma: PrismaClient | undefined;
-}
--- middleware.ts
+++ middleware.ts
@@ -1,41 +1,44 @@
-import authConfig from "@/auth.config"
-import NextAuth from "next-auth"
-import { publicRoutes, apiAuthPrefix, authRoutes, DEFAULT_LOGIN_REDIRECT } from "./routes"
+import authConfig from "@/auth.config";
+import NextAuth from "next-auth";
+import {
+  publicRoutes,
+  apiAuthPrefix,
+  authRoutes,
+  DEFAULT_LOGIN_REDIRECT,
+} from "./routes";
 
-const { auth } = NextAuth(authConfig)
-
-
+const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
-const {nextUrl} = req;
-const isLoggedIn = !!req.auth;
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
 
-const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
-const isPiblicRoute = publicRoutes.includes(nextUrl.pathname)
-const isAuthRoute = authRoutes.includes(nextUrl.pathname)
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPiblicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-if(isApiAuthRoute) {
-  return null
-}
-
-if(isAuthRoute) {
-  if(isLoggedIn) {
-    return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT,nextUrl))
+  if (isApiAuthRoute) {
+    return undefined;
   }
-  return null;
-}
 
-  if(!isLoggedIn && !isPiblicRoute) {
-    return Response.redirect(new URL("/auth/login",nextUrl))
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return undefined;
   }
-return null;
-})
+
+  if (!isLoggedIn && !isPiblicRoute) {
+    return Response.redirect(new URL("/auth/login", nextUrl));
+  }
+  return undefined;
+});
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
-}
+};
