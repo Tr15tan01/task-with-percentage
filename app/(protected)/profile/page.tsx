9d5c23a2dabd9f93535c5fc7e2ad6955486d39dc
tr15tan01 <t.varamashvili@gmail.com>
2024-10-28 23:27:20 +0400

    works with google auth

--- app/(protected)/profile/page.tsx
+++ app/(protected)/profile/page.tsx
@@ -3,26 +3,23 @@
 // import { useCurrentUser } from "@/hooks/use-current-user";
 import { signOut } from "next-auth/react";
 import { useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
 
 const ProfilePage = () => {
   const { data: session, status } = useSession();
-  const router = useRouter();
   console.log("session is ", session, status);
 
-  useEffect(() => {
-    console.log("session is", session);
-    console.log("satus is ", status);
-  }, [session, status]);
+  if (status === "loading") return <p>Loading...</p>;
+
+  if (!session) return <p>Not logged in</p>;
 
   const onClick = () => signOut();
+
   return (
     <div className=" text-gray-600 h-full">
       {/* {JSON.stringify(user)} */}
       <div className="m-3">
         <h2>User is: </h2>
+        <p>Logged in as {session.user?.name}</p>;
       </div>
       <button className="p-3 rounded-sm" onClick={onClick}>
         Sign Out
